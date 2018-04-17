e7b6117b37966052b162c6077b9d2651fcd8fe94
G_Grant <grantcolemans@gmail.com>
2018-04-17 10:11:52 +0800

    使用browserAction

--- js/background.js
+++ js/background.js
@@ -1,8 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
-    // 当 tabs 发生改变时候，对改变的 tab 显示 pageAction
-    chrome.tabs.onUpdated.addListener((tabId)=>{
-        chrome.pageAction.show(tabId);
-    })
     chrome.tabs.onActivated.addListener((activeInfo)=>{
         chrome.tabs.get(activeInfo.tabId, (tab)=>{
             chrome.storage.sync.set({url: tab.url})
